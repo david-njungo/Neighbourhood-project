cae2bf1c6e053bf174c69e90e0ab054713f36cb7
David Njungo <davidnjungo3@gmail.com>
2021-09-30 10:48:35 +0300

    create neighbourhood class

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Neighhood(models.Model):
+    hoodname = models.CharField(max_length =30)
+    location = models.CharField(max_length =30)
+    occupantscount = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.hoodname
+    class Meta:
+        ordering = ['hoodname'] 
+
+   
