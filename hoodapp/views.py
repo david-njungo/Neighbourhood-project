06abd54308a65fbf675721f2b5372b9a55708e5d
David Njungo <davidnjungo3@gmail.com>
2021-09-30 21:19:48 +0300

    create business search views

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -3,4 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    
     return render(request, 'home.html')
+
+def search_results(request):
+
+    if 'business' in request.GET and request.GET["business"]:
+        search_term = request.GET.get("business")
+        searched_businesses= Business.search_by_name(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"businesses": searched_businesses})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
