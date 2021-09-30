a856ad31279bcb72bee0891f222c5c7981d7f0b5
David Njungo <davidnjungo3@gmail.com>
2021-09-30 23:45:52 +0300

    deploy to heroku

--- hood/settings.py
+++ hood/settings.py
@@ -18,7 +18,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+SECRET_KEY='django-insecure-vn*k0-da6jd#ax=arlsue8r0+cnejwn@qw_fesk14tm@$f)tdb'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
