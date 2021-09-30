91fdee60a9b8bbed3fe101cbcec914bd7f1d288d
David Njungo <davidnjungo3@gmail.com>
2021-09-30 13:49:37 +0300

    add business class

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Neighhood(models.Model):
-    hoodname = models.CharField(max_length =30)
+    Neighbourhood_name = models.CharField(max_length =30)
     location = models.CharField(max_length =30)
-    occupantscount = models.CharField(max_length =30)
+    occupantscount = models.IntegerField(null=True, blank=True)
+    admin = models.ForeignKey('User', on_delete=models.CASCADE))
 
     def __str__(self):
         return self.hoodname
@@ -18,3 +20,18 @@ class Neighhood(models.Model):
     # def get_project(cls):
     #     projects = cls.objects.all()
     #     return projects
+class User(models.Model):
+    user_name = models.CharField(max_length=150,blank=True)
+    Neighbourhood = models.ForeignKey('Neighhood',on_delete=models.CASCADE)
+    email_address = models.EmailField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Business(models.Model):
+    business_name = models.CharField(max_length=150,blank=True)
+    business_emailaddress = models.EmailField()
+    admin = models.ForeignKey('User', on_delete=models.CASCADE))
+    Neighbourhood = models.ForeignKey('Neighhood',on_delete=models.CASCADE)
+    
